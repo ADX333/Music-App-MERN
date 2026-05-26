caa8a023e8a67ca1dae8d8b5cf80ee0b798d02b3
Anshuman <anshumandixit333@gmail.com>
2026-05-26 16:37:23 +0530

    DB setup

--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URI)
-  console.log("DB Connected")  
+  try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("DB Connected")  
+  }
+  catch(error){
+    console.error("Database Connection Failed!", error);
+  }
 }
-
-module.exports=connectDB
+module.exports=connectDB;
