ec61a33f9d0e408df17ad48c95a69090c0123880
Anshuman <anshumandixit333@gmail.com>
2026-05-28 21:33:39 +0530

    Register_full-jwt,bcrypt

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,44 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const router=express.Router();
+const bcrypt=require('bcryptjs')
+
+async function registerUser(req,res){
+  const {username,email,password,role="user"}=req.body;
+
+   const userExists=await userModel.findOne({
+    $or:[
+    {username:username},
+    {email:email}
+   ]
+  })
+
+  if (userExists){
+    return res.status(409).json({message:"User already exists"})
+  }
+
+  const hash=await bcrypt.hash(password, 10);
+
+  const user=await userModel.create({
+    username,
+    email,
+    password:hash,
+    role
+  })
+  const token=jwt.sign({
+    id:user._id,
+    role: user.role
+  },
+     process.env.JWT_TOKEN)
+
+  res.cookie("token", token);
+  res.status(201).json({
+    message:"User Registered Succesfully",
+    user:{
+      id:user._id,
+      username:user.username,
+      email: user.email,
+      role : user.role,
+    }
+  })
+}
