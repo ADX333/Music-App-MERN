15b7782b9f3c7960b25abc28937ce2d1ffc31960
Anshuman <anshumandixit333@gmail.com>
2026-06-06 00:32:27 +0530

    small changes

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,8 +1,28 @@
-
-
+const musicModel = require("../models/music.model");
+const jwt=require("jsonwebtoken");
+const imagekit=require
 
 async function addMusic(req,res){
+  const token=req.cookies.token;
+  if(!token) return res.status(403).json({
+      message:"Invalid Request! User not allowed to add music"
+    })
+  try{
+    const decoded=jwt.verify(token,process.env.JWT_TOKEN);
+  }
+  catch(err){
+    return res.status(401).json({
+      message:"Invalid Request! User not allowed to add music"
+  })
+  const role=decoded.role;
+   
+  if(role!='artist'){
+    return res.status(403).json({
+      message:"Invalid Request! User not allowed to add music"
+    })
+  const song=await musicModel.create()
+  }
+  }}
 
-}
 
 module.exports=addMusic;
---
+++ src/services/storage.service.js
@@ -0,0 +1,18 @@
+const {ImageKit}=require("@imagekit/nodejs");
+const { URLEndpoints } = require("@imagekit/nodejs/resources/accounts/url-endpoints.js");
+
+const ImageKitClient= new ImageKit({
+  privateKey:process.env.IMAGEKIT_PVT_KEY,
+  
+})
+
+async function uploadFile(file){
+  const result=ImageKitClient.files.upload({
+    file,
+    fileName:"music_"+ Date.now(),
+    folder:"backend/music"
+  })
+  return result;
+}
+
+module.exports={uploadFile}
