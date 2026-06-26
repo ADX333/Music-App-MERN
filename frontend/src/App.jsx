a93d1cba2ad5833175621ca08958f9a3c4c9c5d7
Anshuman <anshumandixit333@gmail.com>
2026-06-26 15:27:02 +0530

    Home page working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {Routes,Route} from 'react-router-dom'
 import Login from './components/Auth/Login'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 
 export default function App(){
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [songs, setSongs]=useState([]);
+
+  useEffect(()=>{
+    const fetchSongs=async()=>{
+      try{
+      const getSongs=await axios.get('http://localhost:3000/api/music/getMusic', {withCredentials:true})
+      console.log(getSongs.data)
+      setSongs(getSongs.data.songs)
+      }catch(err){
+        console.log(err)
+      }
+  }
+  fetchSongs()
+
+},[])
+  
   return (
-    <div>Home</div>
+    <div>
+      <p>Songs</p>
+      {songs.map((song)=>(
+        <div key={song._id}>
+          <p>{song.title}</p>
+          <audio src={song.uri} controls></audio>
+        </div>
+      ))}
+    </div>
   )
 }
 
