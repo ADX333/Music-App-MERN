befdc195c62e26c418db6bc48edfb0165303b9bd
Anshuman <anshumandixit333@gmail.com>
2026-06-30 18:33:08 +0530

    Upload Page Complete

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import backGround from '../assets/backg1.png';
+import musicIcon from '../assets/Icon3.png';
 
 function Upload() {
   const [title,setTitle]=useState("");
@@ -28,14 +30,30 @@ function Upload() {
   }
   
   return (
-    <div className="bg-black h-screen">
-      <img src=""></img>
-      <form onSubmit={handleSubmit}>
-        <input value={title} placeholder='Enter the title' onChange={handleTitle}className='border-purple-700 rounded-lg'></input>
-        <input type="file" placeholder="Upload the file" onChange={handleFile}
-        className='border-purple-700 rounded-lg'></input>
-        <button>Upload</button>
+    <div className=" relative h-screen bg-cover bg-center" style={{ backgroundImage: `url(${backGround})` }}>
+      <div className="absolute top-6 left-6 flex items-center gap-2">
+      <img src={musicIcon} alt="Musico logo" className="w-24 h-16" />
+      <p className="text-white font-bold">Musico</p>
+      </div>
+      <div className="flex flex-col h-full justify-center items-center font-['Roboto_Condensed']">
+      <div className="w-96 bg-white/5 backdrop-blur-md rounded-2xl border border-white/20">
+      <div className="flex h-16 rounded-t-2xl bg-purple-700 justify-end">
+        <div className="text-right">
+        <h1 className="text-white font-bold ml-4 mt-2 text-xl mr-2">Musico&reg;</h1>
+        
+        <p className="text-white mr-2 text-xl ">Upload your Music</p>
+        </div>
+      </div>
+      <div className="p-10 gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input className="border border-purple-700 rounded-sm p-2 w-full bg-gray-800 hover:scale-[1.01] transition-all duration-300 text-white placeholder-gray-500" value={title} placeholder='Enter the title' onChange={handleTitle}></input>
+        <input className="border border-purple-700 rounded-sm p-2 w-full bg-gray-800 hover:scale-[1.01] transition-all duration-300 text-white placeholder-gray-500" type="file" placeholder="Upload the file" onChange={handleFile}
+        ></input>
+        <button className="bg-purple-700 text-white rounded-xl p-3 w-full hover:scale-105 transition-all duration-300 ">Upload</button>
       </form>
+      </div>
+      </div>
+      </div>
     </div>
   )
 }
